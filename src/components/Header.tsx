24b95a921b04f7c395c944029f900e9911d3c0f2
zerooo111 <zero@fermilabs.xyz>
2025-07-13 10:47:59 +0530

    fix height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 	return (
 		<header className="sticky top-0 w-full left-0 bg-dark-forest/60 backdrop-blur-lg border-b border-rock/30 z-10 gap-2 h-14 flex items-center">
 			<nav className="flex flex-1 h-full justify-between items-center gap-1 text-xl md:border-x border-rock/20 font-medium container-2xl px-4">
-				<div className="flex items-center md:border-x border-rock/20 md:pl-2 md:pr-4 group hover:text-amber-200 gap-1">
+				<div className="flex items-center md:border-x h-full border-rock/20 md:pl-2 md:pr-4 group hover:text-amber-200 gap-1">
 					<Logo className="w-8 h-8 md:w-10 md:h-10 group-hover:rotate-360 duration-500" />
 					<a
 						className="text-2xl md:text-3xl font-display tracking-wide"
