73e853c9884a3fbbe41210600e2bff6bfb4b16e1
zerooo111 <zero@fermilabs.xyz>
2025-07-14 18:36:23 +0530

    update links

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
-	DOCS: "/",
-	APP: "https://devnet1.fermilabs.xyz",
+	DOCS: "https://docs.fermilabs.xyz",
+	APP: "https://testnet.fermilabs.xyz",
 	WHITEPAPER: "/whitepaper.pdf",
 	TWITTER: "https://x.com/FermiLabs",
 	DISCORD: "https://discord.gg/kNcktKSk7u",
